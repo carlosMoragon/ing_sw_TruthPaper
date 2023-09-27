8f8603b34b70bb51694aab5b5f8a4b653409b1f2
Orianna Milone <oriannamilone.s@gmail.com>
2023-09-27 08:47:25 +0200

    DDBB Manager v0

---
+++ source/database/DBManager.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask_mysqldb import MySQL
+from app import app
+#from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# MySQL Connection
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/base_datos'
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'base_datos'
+connection = MySQL(app)
+
+#db = SQLAlchemy(app)
+
+
+#PENDIENTE: HACER EL TEST Y LA CONEXION
+#Flask_mysql manages the opening and closing connection (No need to generate a method, to close it)
+
+
+
+
