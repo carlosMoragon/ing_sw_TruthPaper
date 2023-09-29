38fd304a2dc63030dd278480da4e736ce20695e4
Carlos Moragon <cmoragoncorella@gmail.com>
2023-09-29 20:12:47 +0200

    Terminado backend webScrapping de antena3 y empezando lasexta

--- source/modules/webScrapping.py
+++ source/modules/webScrapping.py
@@ -3,9 +3,12 @@ import requests
 from bs4 import BeautifulSoup
 import classes as cl
 from typing import List
+from datetime import datetime
+
+
 # Implementando el webScrapping de antena3noticias
 def get_antena3news() -> List[cl.News]:
-    # new = cl.News("title", "images", "resumen", "url", "comentarios", "fecha", 1, "owner")
+    # new = cl.News(title, image, summary, url, date, owner)
     antena3 = requests.get("https://www.antena3.com/noticias/")
     antena3_structure = BeautifulSoup(antena3.text, 'lxml')
 
@@ -21,12 +24,24 @@ def get_antena3news() -> List[cl.News]:
         titles.append(a_texts)
 
     url_imgs = [article.find('img').get('src') for article in articles]
-    #print("a")
-    for url in url_imgs:
-        print(f"{url}\n")
-    #print(link_news)
-    #print(titles)
+    # print("a")
+    # for url in url_imgs:
+    #    print(f"{url}\n")
+    # print(link_news)
+    # print(titles)
 
-    return None
+    news = []
+    for i in range(len(titles)):
+        news.append(cl.News(titles[i][1], url_imgs[i], "", link_news[i], datetime.now().strftime(f"%Y-%m-%d"), "Antena 3 Noticias").__str__())
+    # cl.News(titles, url_imgs, "", link_news, datetime.now().strftime(f"%Y-%m-%d"), "Antena 3 Noticias")
+    print(news)
+    return news
+
 
 get_antena3news()
+
+
+def get_lasextanews():
+
+    lasexta = requests.get("https://www.lasexta.com/noticias/")
+    lasexta_structure = BeautifulSoup(lasexta.text, 'lxml')
