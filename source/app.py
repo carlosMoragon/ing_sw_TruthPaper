42aa72b581a6c9b516c96501a52b84724e702366
Carlos Moragon <cmoragoncorella@gmail.com>
2023-09-30 12:54:10 +0200

    hecha una pequeña comprobación con gui del webscrapping

--- source/app.py
+++ source/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from modules import web_scrapping as ws
 
 app = Flask(__name__)
@@ -9,11 +9,24 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/pruebaArticulos')
+def prueba_articulos():
+    news = ws.get_lasextanews()
+    news = news + ws.get_antena3news()
+    data = {
+        'imgs' : [new.get_image() for new in news],
+        'titles' : [new.get_title() for new in news],
+        'urls' : [new.get_url() for new in news]
+    }
+
+    return render_template('pruebaArticulosFunc.html', data=data)
+
+
 # Crear una etiqueta {}
 for etiq in ws.get_lasextanews():
     etiq.get_image()
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
