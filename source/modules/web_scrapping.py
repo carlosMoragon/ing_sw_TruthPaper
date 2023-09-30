ff16260b448bdeb4d3aa1f51fd3ef5ee340aa3c9
Carlos Moragon <cmoragoncorella@gmail.com>
2023-09-30 11:59:00 +0200

    voy a empezar con las pruebas de integración a front

--- source/modules/web_scrapping.py
+++ source/modules/web_scrapping.py
@@ -1,22 +1,21 @@
 import requests
 #  pip install beautifulsoup4
 from bs4 import BeautifulSoup
-import classes as cl
+from modules import classes as cl
 from typing import List
 from datetime import datetime
 
 
-def build_new(titles, urls, imgs, owner: str) -> List[cl.News]:
+def build_news(titles, urls, imgs, owner: str) -> List[cl.News]:
+    # print(f"titles.len: {len(titles)}, urls.len: {len(urls)}, imgs.len: {len(imgs)}\n")
     news = []
     for i in range(len(titles)):
-        news.append(cl.News(titles[i][1], imgs[i], "", urls[i], datetime.now().strftime(f"%Y-%m-%d"), owner))
-
-    print(news)
+        # print(f"{titles[i]}\n")
+        news.append(cl.News(titles[i], imgs[i], "", urls[i], datetime.now().strftime(f"%Y-%m-%d"), owner))
 
     return news
 
 
-# Implementando el webScrapping de antena3noticias
 def get_antena3news() -> List[cl.News]:
     # new = cl.News(title, image, summary, url, date, owner)
     antena3 = requests.get("https://www.antena3.com/noticias/")
@@ -30,17 +29,12 @@ def get_antena3news() -> List[cl.News]:
     titles = []
     for article in articles:
         a_tags = article.find_all('a')
-        a_texts = [a_tag.text for a_tag in a_tags]
+        a_texts = [a_tag.text for a_tag in a_tags[len(a_tags)-1]]
         titles.append(a_texts)
 
     url_imgs = [article.find('img').get('src') for article in articles]
-    # print("a")
-    # for url in url_imgs:
-    #    print(f"{url}\n")
-    # print(link_news)
-    # print(titles)
-
-    return build_new(titles=titles, urls=link_news, imgs=url_imgs, owner='antena3noticias')
+    # [title[1] for title in titles]
+    return build_news(titles=titles, urls=link_news, imgs=url_imgs, owner='antena3noticias')
 
 
 def get_lasextanews() -> List[cl.News]:
@@ -58,10 +52,5 @@ def get_lasextanews() -> List[cl.News]:
         h2_texts = [h2_tag.text for h2_tag in h2_tags]
         titles.append(h2_texts)
 
-    print(titles)
+    return build_news(titles=titles, urls=link_news, imgs=url_imgs, owner='LaSexta')
 
-    return build_new(titles=titles, urls=link_news, imgs=url_imgs, owner='LaSexta')
-
-
-# get_antena3news():
-# get_lasextanews()
