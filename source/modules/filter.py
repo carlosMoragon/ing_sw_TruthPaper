4e0ef724402f3a3fddf8394ee53bde66537e7a3e
Carlos Moragon <cmoragoncorella@gmail.com>
2023-09-30 16:37:13 +0200

    NO COGER COMO BACKUP, arreglando filter

--- source/modules/filter.py
+++ source/modules/filter.py
@@ -1,9 +1,10 @@
 import classes as cl
+# from modules import classes as cl
 from typing import List
 import re
 import requests
 
-# from modules import classes as cl
+import web_scrapping as ws
 
 
 def filter_by_words(search: str, news: List[cl.News]) -> List[cl.News]:
@@ -17,7 +18,8 @@ def filter_by_words(search: str, news: List[cl.News]) -> List[cl.News]:
 def _search_title(word: str, news: List[cl.News]) -> List[cl.News]:
     matches = []
     for new in news:
-        if re.match(word, new.get_title()):
+        # Puede que new.get_title() este devolviendo una List[str]
+        if re.match(r'{}'.format(word), new.get_title()):
             matches.append(new)
 
     return matches
@@ -26,7 +28,16 @@ def _search_title(word: str, news: List[cl.News]) -> List[cl.News]:
 def _search_content(word: str, news: List[cl.News]) -> List[cl.News]:
     matches = []
     for new in news:
-        if re.match(word, requests.get(new.get_url()).text):
+        if re.match(r'{}'.format(word), requests.get(new.get_url()).text.strip()):
             matches.append(new)
 
     return matches
+
+
+lista = filter_by_words("a", ws.get_lasextanews() + ws.get_antena3news())
+if lista:
+    print("Lista no vacia\n")
+    print(lista)
+else:
+    print("lista vacia")
+
--- source/modules/web_scrapping.py
+++ source/modules/web_scrapping.py
@@ -1,7 +1,8 @@
 import requests
 #  pip install beautifulsoup4
 from bs4 import BeautifulSoup
-from modules import classes as cl
+# from modules import classes as cl
+import classes as cl
 from typing import List
 from datetime import datetime
 
@@ -29,7 +30,7 @@ def get_antena3news() -> List[cl.News]:
     titles = []
     for article in articles:
         a_tags = article.find_all('a')
-        a_texts = [a_tag.text for a_tag in a_tags[len(a_tags)-1]]
+        a_texts = [a_tag.text.strip() for a_tag in a_tags[len(a_tags)-1]]
         titles.append(a_texts)
 
     # url_imgs = [article.find('img').get('src') for article in articles]
@@ -57,7 +58,7 @@ def get_lasextanews() -> List[cl.News]:
     titles = []
     for article in articles:
         h2_tags = article.find_all('h2', class_='titular t3')
-        h2_texts = [h2_tag.text for h2_tag in h2_tags]
+        h2_texts = [h2_tag.text.strip() for h2_tag in h2_tags]
         titles.append(h2_texts)
 
     return build_news(titles=titles, urls=link_news, imgs=url_imgs, owner='LaSexta')
