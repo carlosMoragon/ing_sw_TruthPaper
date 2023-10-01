1965186a1491b4bc8e0ee7f0ac3fae4a14a86656
Carlos Moragon <cmoragoncorella@gmail.com>
2023-10-01 13:36:44 +0200

    terminado filtrado

--- source/modules/filter.py
+++ source/modules/filter.py
@@ -12,7 +12,7 @@ def filter_by_words(search: str, news: List[cl.News]) -> List[cl.News]:
     words = search.split(" ")
     all_news = []
     for word in words:
-        all_news = all_news + _search_title(word, news)# + _search_content(word, news)
+        all_news = all_news + _search_title(word, news) + _search_content(word, news)
     return list(set(all_news))
 
 
@@ -29,19 +29,7 @@ def _search_title(word: str, news: List[cl.News]) -> List[cl.News]:
 def _search_content(word: str, news: List[cl.News]) -> List[cl.News]:
     matches = []
     for new in news:
-        if re.search(word, requests.get(new.get_url()).text.strip()):
+        if re.search(word, ws.get_content(new.get_url())):
             matches.append(new)
 
     return matches
-
-"""
-lista = filter_by_words("a", ws.get_lasextanews() + ws.get_antena3news())
-if lista:
-    print("Lista no vacia\n")
-    print(lista)
-else:
-    print("lista vacia")
-"""
-
-
-
