8c6cef07fb26fefb3d333a6d030d8bbadf15a277
Carlos Moragon <cmoragoncorella@gmail.com>
2023-10-03 21:01:53 +0200

    hecho los test pero hay un problema con el venv y no se pueden correr

--- source/tests/tests.py
+++ source/tests/tests.py
@@ -70,3 +70,24 @@ class TestWebScrapping(unittest.TestCase):
             self.assertIsNotNone(news.get_url())
             self.assertIsNotNone(news.get_date())
             self.assertEqual(news.get_owner(), 'LaSexta')
+
+
+class TestVerificationAlgorithms(unittest.TestCase):
+    def test_verification_date_good(self):
+        self.assertEqual(cl.validate_date("2023-11-19"), True)
+
+    def test_verification_date_bad(self):
+        self.assertEqual(cl.validate_date("19-2023-11"), False)
+        self.assertEqual(cl.validate_date("19-11-2023"), False)
+        self.assertEqual(cl.validate_date("aaaaaaaaa"), False)
+        self.assertEqual(cl.validate_date("2023/11/19"), False)
+
+    def test_verification_password_good(self):
+        self.assertEqual(cl.validate_password("Holamundo1234@"), True)
+
+    def test_verification_password_bad(self):
+        self.assertEqual(cl.validate_password("1234"), False)
+        self.assertEqual(cl.validate_password("HolaMundo1234"), False)
+        self.assertEqual(cl.validate_password("HolaMundo@"), False)
+        self.assertEqual(cl.validate_password("HolaMundo123@"), False)
+        self.assertEqual(cl.validate_password("molamundo1234@"), False)
