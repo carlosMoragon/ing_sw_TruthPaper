65aa6c48ea59e4f7ba70402102c1e12ba95b6c0a
carlosMoragon <95918162+carlosMoragon@users.noreply.github.com>
2023-10-31 05:48:13 -0700

    Update instalacion.sh

--- instalacion.sh
+++ instalacion.sh
@@ -1,5 +1,10 @@
 sudo apt install git
 git clone https://github.com/carlosMoragon/ing_sw_TruthPaper.git
 cd ing_sw_TruthPaper
+git branch pruebaIntegracion
+git checkout pruebaIntegracion
+git config --global user.email "cmoragoncorella@gmail.com"
+git config --global user.name "carlos"
+git pull origin pruebaIntegracion
 sudo apt  install docker.io 
 docker build -t flaskapp .
