59499624b9902b1f8f031b5e1fe68342c4e36cd5
carlos moragon <carlos.moragoncorella@usp.ceu.es>
2023-11-05 09:41:14 -0800

    actualizacion de instalacion

--- instalacion.sh
+++ instalacion.sh
@@ -1,10 +1,9 @@
-sudo apt install git
-git clone https://github.com/carlosMoragon/ing_sw_TruthPaper.git
-cd ing_sw_TruthPaper
-git branch pruebaIntegracion
-git checkout pruebaIntegracion
-git config --global user.email "cmoragoncorella@gmail.com"
-git config --global user.name "carlos"
-git pull origin pruebaIntegracion
-sudo apt  install docker.io 
-docker build -t flaskapp .
+if [ -x "$(command -v docker)" ]; then
+  echo "Docker ya está instalado en el sistema."
+  exit 0
+else 
+  sudo apt  install docker.io 
+fi
+sudo docker pull cmoragon/flaskapp:v1
+
+sudo docker run -it -p 4000:4000 -d cmoragon/flaskapp:v1
